842bab3817f82b7f7c1d16d9da272e848e81599e
07150807 <497115679@qq.com>
2016-12-21 18:14:29 +0800

    si1

---
+++ app/src/main/java/cn/edu/gdmec/s07150843/myguard/m3communicationguard/entity/ContactInfo.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.s07150843.myguard.m3communicationguard.entity;
+
+/**
+ * Created by asus-c on 2016/12/20.
+ */
+public class ContactInfo {
+    public String id;
+    public String name;
+    public String phone;
+}
