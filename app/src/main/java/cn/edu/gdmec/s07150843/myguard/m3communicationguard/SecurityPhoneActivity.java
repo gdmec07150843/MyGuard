666a06737ff99ad4dded8e0325f1f135f5a5b9b3
07150807 <497115679@qq.com>
2016-12-22 17:32:17 +0800

    hhh

--- app/src/main/java/cn/edu/gdmec/s07150843/myguard/m3communicationguard/SecurityPhoneActivity.java
+++ app/src/main/java/cn/edu/gdmec/s07150843/myguard/m3communicationguard/SecurityPhoneActivity.java
@@ -139,6 +139,7 @@ implements View.OnClickListener {
         }
             }
 
+            
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
                                  int totalItemCount) {
